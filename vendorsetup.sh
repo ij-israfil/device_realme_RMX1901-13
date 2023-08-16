14594b265d6db5f8fe2dcb6b8042692d1f639249
ij-israfil~ <israfilkhan494@gmail.com>
2023-08-16 08:02:00 +0600

    Update Vendor

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,19 +1,9 @@
-# Vendor Tree
-echo 'Cloning Vendor tree [1/5]'
-git clone git@github.com:ashim-anwar/vendor_realme_RMX1901-13.git -b 13 vendor/realme/RMX1901
-
-# Kernel Tree
-echo 'Cloning Kernel tree [2/5]'
-git clone git@github.com:ashim-anwar/kernel_realme_RMX1901.git -b x.337-KSU kernel/realme/RMX1901
 
 #Syncing KernelSU to latest source and avoid a conflict
-echo "Syncing KernelSU to latest source [3/5]"
+echo "Syncing KernelSU to latest source [1/2]"
 cd kernel/realme/RMX1901 && git submodule sync && git submodule update --init --recursive && cd KernelSU && git checkout main && git pull && touch Android.mk && cd ../../../..
 
 # Realme In-Screen Proximity Patch
-echo 'Patching Realme In-Screen Proximity [4/5]'
-cd frameworks/base && git fetch git@github.com:ashim-anwar/proximity_patch.git 13 && git cherry-pick 30ea88924980002d94fb8e2bf4a5016905ca6914 && cd ../..
+echo 'Patching Realme In-Screen Proximity [2/2]'
+cd frameworks/base && git fetch https://github.com/ij-israfil/proximity_patch.git && git cherry-pick afb38a0f9f9a612b13b12b901ab9b7f0fffae3de && cd ../..
 
-# Inherit Pocket Mode
-echo ' Cloning PocketMode [5/5]'
-git clone git@github.com:ashim-anwar/packages_apps_PocketMode.git -b 13 packages/apps/PocketMode

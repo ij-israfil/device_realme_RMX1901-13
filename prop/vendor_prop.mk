d6e771ea06617bf7ce14a70e4fe5f35a97c8a1d5
ij-israfil~ <israfilkhan494@gmail.com>
2023-08-16 07:14:54 +0600

    Remove 3 framework Render
    no need in qpr3

--- prop/vendor_prop.mk
+++ prop/vendor_prop.mk
@@ -164,7 +164,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.vulkan=adreno \
     debug.sdm.support_writeback=0 \
     ro.config.avoid_gfx_accel=true \
-    ro.hwui.render_ahead=3 \
     ro.surface_flinger.set_idle_timer_ms=80 \
     ro.surface_flinger.set_touch_timer_ms=200 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \

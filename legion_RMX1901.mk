c3dcdedb3f394a592504db079d3c7cf83563ad07
ij-israfil~ <israfilkhan494@gmail.com>
2023-08-28 11:33:40 +0600

    Bring Legion OS

--- legion_RMX1901.mk
+++ legion_RMX1901.mk
@@ -8,24 +8,22 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Evolution-X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/legion/config/common.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
-EVO_BUILD_TYPE := COMMUNITY
+LEGION_GAPPS := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_SUPPORTS_QUICK_TAP := false
-EXTRA_UDFPS_ANIMATIONS := true
-TARGET_USES_MINI_GAPPS := true
+TARGET_SUPPORTS_QUICK_TAP := true
 # Inherit from RMX1901 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := evolution_RMX1901
+PRODUCT_NAME := legion_RMX1901
 PRODUCT_MODEL := Realme X
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo

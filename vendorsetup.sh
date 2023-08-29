070428d960ba7b9b6fd9d60b0f7619ff2e5b0fd9
ij-israfil~ <israfilkhan494@gmail.com>
2023-08-29 22:49:31 +0600

    Update Proximity Patch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,5 +5,4 @@ cd kernel/realme/RMX1901 && git submodule sync && git submodule update --init --
 
 # Realme In-Screen Proximity Patch
 echo 'Patching Realme In-Screen Proximity [2/2]'
-cd frameworks/base && git fetch https://github.com/ij-israfil/proximity_patch.git && git cherry-pick afb38a0f9f9a612b13b12b901ab9b7f0fffae3de && cd ../..
-
+cd frameworks/base && git fetch https://github.com/ij-israfil/proximity_patch.git && git cherry-pick de630e735cdbb68a606d3bbbf8b1674be31bab2d && cd ../..

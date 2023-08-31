7e116c7c1297b466314d4f613657f4ad7daf22d9
ij-israfil~ <israfilkhan494@gmail.com>
2023-08-31 12:03:13 +0600

    Bring Cherish OS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_RMX1901.mk
+    $(LOCAL_DIR)/cherish_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_RMX1901-userdebug \
-    evolution_RMX1901-eng
+    cherish_RMX1901-userdebug \
+    cherish_RMX1901-eng
--- cherish_RMX1901.mk
+++ cherish_RMX1901.mk
@@ -8,16 +8,20 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Evolution-X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common Cherish OS stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
-EVO_BUILD_TYPE := COMMUNITY
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_USES_BLUR := true
+USE_PIXEL_CHARGING := true
 TARGET_SUPPORTS_QUICK_TAP := true
 EXTRA_UDFPS_ANIMATIONS := true
 TARGET_USES_MINI_GAPPS := true
+TARGET_INCLUDE_STOCK_ARCORE := true
 
 # Inherit from RMX1901 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -25,17 +29,21 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := evolution_RMX1901
+PRODUCT_NAME := cherish_RMX1901
 PRODUCT_MODEL := Realme X
+
+# maintainer
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=м∂_ιѕяαƒιℓ
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cheetah-user 13 TQ3A.230705.001.A1 10217028 release-keys" \
+    PRIVATE_BUILD_DESC="cheetah-user 13 TQ3A.230805.001 10316531 release-keys" \
     PRODUCT_NAME="RMX1901"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/cheetah/cheetah:13/TQ3A.230705.001.A1/10217028:user/release-keys
+BUILD_FINGERPRINT := google/cheetah/cheetah:13/TQ3A.230805.001/10316531:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)

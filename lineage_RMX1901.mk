883a7c459dd6bbcea7bbac5c6ef1320d9844948d
Ij Israfil <86546896+ij-israfil@users.noreply.github.com>
2023-09-13 15:57:39 +0600

    Bring Alphadroid

--- lineage_RMX1901.mk
+++ lineage_RMX1901.mk
@@ -8,34 +8,40 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Evolution-X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common Alphadroid stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# maintainer flag
+ALPHA_MAINTAINER := м∂_ιѕяαƒιℓ
+WITH_GAPPS := true
+
+EXTRA_UDFPS_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1080
-EVO_BUILD_TYPE := COMMUNITY
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_SUPPORTS_QUICK_TAP := true
-EXTRA_UDFPS_ANIMATIONS := true
-TARGET_USES_MINI_GAPPS := true
+TARGET_USES_BLUR := true
+TARGET_BUILD_APERTURE_CAMERA := false
+
+
 # Inherit from RMX1901 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := evolution_RMX1901
+PRODUCT_NAME := lineage_RMX1901
 PRODUCT_MODEL := Realme X
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cheetah-user 13 TQ3A.230805.001 10316531 release-keys" \
-    PRODUCT_NAME="RMX1901"
+PRIVATE_BUILD_DESC="RMX1901-user 11 RKQ1.201217.002 1651205774457 release-keys" \
+TARGET_DEVICE=RMX1901 \
+TARGET_PRODUCT=RMX1901
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/cheetah/cheetah:13/TQ3A.230805.001/10316531:user/release-keys
+BUILD_FINGERPRINT := realme/RMX1901/RMX1901:11/RKQ1.201217.002/1651205774457:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)

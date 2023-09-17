b7ce0fc5a9dad43995fd22c0bb2e0967b8df1977
Ij Israfil <86546896+ij-israfil@users.noreply.github.com>
2023-09-18 04:28:54 +0600

    use default fingerprint

--- superior_RMX1901.mk
+++ superior_RMX1901.mk
@@ -43,11 +43,12 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.spos.maintainer=м∂_ιѕяαƒιℓ
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cheetah-user 13 TQ3A.230805.001 10316531 release-keys" \
-    PRODUCT_NAME="RMX1901"
+PRIVATE_BUILD_DESC="RMX1901-user 11 RKQ1.201217.002 1651205774457 release-keys" \
+TARGET_DEVICE=RMX1901 \
+TARGET_PRODUCT=RMX1901
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/cheetah/cheetah:13/TQ3A.230805.001/10316531:user/release-keys
+BUILD_FINGERPRINT := realme/RMX1901/RMX1901:11/RKQ1.201217.002/1651205774457:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)

dd75774213853acd11e6fd956116be1b66682226
Ij Israfil <86546896+ij-israfil@users.noreply.github.com>
2023-09-22 22:25:08 +0600

    Bring PixelOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1901.mk
+    $(LOCAL_DIR)/aosp_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX1901-userdebug \
-    lineage_RMX1901-eng
+    aosp_RMX1901-userdebug \
+    aosp_RMX1901-eng

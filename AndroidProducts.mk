284c5d9dda4117dd1834c4e0f549eb47ab0c8d83
Ij Israfil <86546896+ij-israfil@users.noreply.github.com>
2023-09-24 01:41:12 +0600

    Bring AICP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1901.mk
+    $(LOCAL_DIR)/aicp_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX1901-userdebug \
-    lineage_RMX1901-eng
+    aicp_RMX1901-userdebug \
+    aicp_RMX1901-eng

4ebd4fc6b55571d3ac6879955d016203d018b0cc
Ij Israfil <86546896+ij-israfil@users.noreply.github.com>
2023-09-24 01:40:10 +0600

    Bring AICP

---
+++ aicp_RMX1901.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+
+AICP_BUILDTYPE := UNOFFICIAL
+
+# Inherit from RMX1901 device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := Realme
+PRODUCT_DEVICE := RMX1901
+PRODUCT_MANUFACTURER := Realme
+PRODUCT_NAME := aicp_RMX1901
+PRODUCT_MODEL := Realme X
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+PRIVATE_BUILD_DESC="RMX1901-user 11 RKQ1.201217.002 1651205774457 release-keys" \
+TARGET_DEVICE=RMX1901 \
+TARGET_PRODUCT=RMX1901
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := realme/RMX1901/RMX1901:11/RKQ1.201217.002/1651205774457:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)

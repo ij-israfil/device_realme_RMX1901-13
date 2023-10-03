aa1bc65646341e987bf08f116d88231216516b60
Ij Israfil <86546896+ij-israfil@users.noreply.github.com>
2023-10-03 11:56:39 +0600

    Update device.mk

--- device.mk
+++ device.mk
@@ -105,6 +105,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
+    PRODUCT_SOONG_NAMESPACES += packages/modules/Bluetooth/android/app
 
 # Bootanimation
 TARGET_SCREEN_HEIGHT := 2340

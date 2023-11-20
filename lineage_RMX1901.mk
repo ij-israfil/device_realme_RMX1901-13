8f9953c068d62cf4d25d6e8b80c138bb3406a508
Ij Israfil <86546896+ij-israfil@users.noreply.github.com>
2023-11-21 04:59:22 +0600

    Bring Crdroid Android 13

--- lineage_RMX1901.mk
+++ lineage_RMX1901.mk
@@ -8,33 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Alphadroid stuff.
+# Inherit some common Crdroid stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# maintainer flag (make Official coz its not showing dev name)
-ALPHA_MAINTAINER := м∂_ιѕяαƒιℓ
-ALPHA_BUILD_TYPE := Official
-TARGET_DISABLE_EPPE := true
-WITH_GAPPS := true
-TARGET_INCLUDE_PIXEL_FRAMEWORK := true
-TARGET_INCLUDE_GBOARD := true
-TARGET_INCLUDE_GOOGLE_DIALER := true
-TARGET_INCLUDE_GOOGLE_MESSAGING := true
-TARGET_INCLUDE_GOOGLE_MARKUP := true
-TARGET_INCLUDE_GOOGLE_SOUNDS := true
-TARGET_INCLUDE_GOOGLE_CONTACTS := true
-TARGET_INCLUDE_GOOGLE_BATTERY_STATS := true
-TARGET_INCLUDE_GOOGLE_APP := false
-TARGET_INCLUDE_GOOGLE_CALCULATOR := true
-TARGET_INCLUDE_GOOGLE_CALENDAR := true
-TARGET_INCLUDE_GOOGLE_CHROME := true
-TARGET_INCLUDE_GOOGLE_DESKCLOCK := true
-TARGET_INCLUDE_GOOGLE_DRIVE := false
-TARGET_INCLUDE_GMAIL := false
-TARGET_INCLUDE_GOOGLE_MAPS := false
-TARGET_INCLUDE_GOOGLE_PHOTOS := false
-TARGET_USE_GOOGLE_TELEPHONY := true
-TARGET_USE_PIXEL_FINGERPRINT := true
 
 TARGET_HAS_UDFPS := true
 EXTRA_UDFPS_ANIMATIONS := true
@@ -43,13 +18,6 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_USES_BLUR := true
-TARGET_EXCLUDES_AUDIOFX := true
-TARGET_BUILD_APERTURE_CAMERA := true
-TARGET_INCLUDE_GOOGLE_SETUP := true
-
-# Debugging stuff.
-TARGET_INCLUDE_MATLOG := false
-TARGET_DEFAULT_ADB_ENABLED := true
 
 # Inherit from RMX1901 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -63,11 +31,11 @@ PRODUCT_MODEL := Realme X
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cheetah-user 13 TQ3A.230901.001 10750268 release-keys" \
+    PRIVATE_BUILD_DESC="husky-user 14 UD1A.231105.004 11010374 release-keys" \
     PRODUCT_NAME="RMX1901"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/cheetah/cheetah:13/TQ3A.230901.001/10750268:user/release-keys
+BUILD_FINGERPRINT := google/husky/husky:14/UD1A.231105.004/11010374:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
